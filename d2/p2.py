4a743ba4c394e584fcd7c06498909fc7b97ee7fe
moknavi <komazec.ivan@gmail.com>
2024-02-15 18:29:05 +0100

    Solved Day 2

--- d2/p2.py
+++ d2/p2.py
@@ -3,22 +3,19 @@ file = open("input.txt", "r")
 valid_passwords = 0
 
 for line in file:
-    chars = 0
+    validity = 0
     line = line.strip()
     line = line.split(":")
     print(line)
-    password = line[1]
+    password = line[1].strip()
     line = line[0].split()
     print(line)
     char = line[1]
     policy = line[0].split("-")
-    print(policy)
     policy = [int(i) for i in policy]
-    for i in password:
-        if i == char:
-            chars += 1
-    if chars >= policy[0] and chars <= policy[1]:
+    print(policy)
+    if (password[policy[0] - 1] == char) ^ (password[policy[1] - 1] == char):
         valid_passwords += 1
-    
+        
 print(valid_passwords)
 file.close()
